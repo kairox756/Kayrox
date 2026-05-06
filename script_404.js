0917342635aad3332ff948247ad45f312c8f2d50
kairox756 <jesusdanielservin3008@outlook.com>
2026-05-05 18:31:47 -0600

    Add files via upload

---
+++ script_404.js
@@ -0,0 +1,18 @@
+// script.js
+function verificarRuta(ruta) {
+  fetch(ruta)
+    .then(response => {
+      if (!response.ok) {
+        window.location.href = "404.html";
+      }
+    })
+    .catch(() => {
+      window.location.href = "404.html";
+    });
+}
+
+// Ejemplo: verificar que exista la página actual
+window.onload = function() {
+  // Aquí puedes comprobar un archivo específico o la ruta actual
+  verificarRuta(window.location.pathname);
+};
